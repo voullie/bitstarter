a1cbb2aeed7f337db3a7f409321aa5fc75e2b485
Ulas A. Albayrak <ulas.albayrak@gmail.com>
2013-07-16 18:49:30 +0000

    Added responsive design framework

--- web.js
+++ web.js
@@ -2,12 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var indexHtml = fs.readFileSync('./index.html').toString();
+//var indexHtml = fs.readFileSync('./index.html').toString();
+
+//var bsCSS = fs.readFileSync('./bootstrap/css/bootstrap-responsive.min.css').toString();
+//	app.get('/bootstrap/css/bootstrap-responsive.min.css', function(request, response) {
+//	response.send(bsCSS);
+//});
+
 app.get('/', function(request, response) {
-  response.send(indexHtml);
+	var indexHtml = fs.readFileSync('./index.html').toString();
+	response.send(indexHtml);
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
